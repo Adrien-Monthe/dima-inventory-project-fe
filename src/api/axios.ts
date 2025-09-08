c73e1ea41374b732594dd1ff6e4589fd345ad086
Adrien Monthe <Steve_Monthe@pkf-researchcenter.com>
2025-09-08 16:47:49 +0100

    feat: Add response interceptor to handle 401 errors with token refresh logic

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -12,4 +12,34 @@ api.interceptors.request.use((config) => {
     return config;
 });
 
+// Response interceptor → check for 401 errors
+api.interceptors.response.use(
+    (response) => response, async (error) => {
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true; // avoid infinite loop
+
+            try {
+                const refresh = localStorage.getItem("refresh");
+                const res = await axios.post("http://localhost:8000/api/token/refresh/", {
+                    refresh,
+                });
+
+                const newAccess = res.data.access;
+                localStorage.setItem("access", newAccess);
+
+                originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+                return api(originalRequest);
+            } catch (err) {
+                localStorage.removeItem("access");
+                localStorage.removeItem("refresh");
+                window.location.href = "/login";
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export default api;
